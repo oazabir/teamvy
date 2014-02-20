294a3eadb49493827d91ea277470c0675b16cbb1
najibcse <najib.cse@gmail.com>
2014-02-20 13:40:36 +0600

    Activity log added

---
+++ PMTool/Models/TaskActivityLog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PMTool.Models
+{
+    public class TaskActivityLog
+    {
+        [Required, Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long TaskActivityLogID { get; set; }
+
+
+        [Display(Name = "Comment")]
+        public string Comment { get; set; }
+
+        [Display(Name = "Choose File")]
+        public string FileUrl { get; set; }
+
+        [Required]
+        public DateTime CreateDate { get; set; }
+
+        [Required]
+        public DateTime ModificationDate { get; set; }
+
+        public long TaskID { get; set; }
+
+    }
+}
