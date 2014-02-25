95e111379ed5024f6ce53c3d81a414cf977e3ca8
mahedee <mahedee.hasan@yahoo.com>
2014-02-25 15:30:24 +0600

    Sync

--- PMTool/Models/Search.cs
+++ PMTool/Models/Search.cs
@@ -7,6 +7,11 @@ namespace PMTool.Models
 {
     public class Search
     {
-        public string SearchParam { get; set; }
+        public List<ProjectStatus> statusList { get; set; }
+        public List<Priority> priorityList { get; set; }
+        public long? SelectedStatusID { get; set; }
+        public long? SelectedPriorityID { get; set; }
+        public long? SelectedProjectID { get; set; }
+
     }
 }
