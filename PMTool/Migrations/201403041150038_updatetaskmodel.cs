f1c5e2378da5c0a238eee5bcbef1b5de558b2c1e
mahedee <mahedee.hasan@yahoo.com>
2014-03-04 18:01:53 +0600

    Change task model

---
+++ PMTool/Migrations/201403041150038_updatetaskmodel.cs
@@ -0,0 +1,16 @@
+namespace PMTool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updatetaskmodel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
