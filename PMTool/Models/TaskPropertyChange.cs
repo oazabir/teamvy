009970be6b204367d7c60a5d5818015ae708cea6
najibcse <najib.cse@gmail.com>
2014-03-18 15:42:21 +0600

    sync

--- PMTool/Models/TaskPropertyChange.cs
+++ PMTool/Models/TaskPropertyChange.cs
@@ -17,6 +17,40 @@ namespace PMTool.Models
             set { isEndtDateChanged = value; }
         }
 
+        DateTime? fromEndDate;
+
+        public DateTime? FromEndDate
+        {
+          get { return fromEndDate; }
+          set { fromEndDate = value; }
+        }
+
+        DateTime? toEndDate;
+
+        public DateTime? ToEndDate
+        {
+            get { return toEndDate; }
+            set { toEndDate = value; }
+        }
+
+        DateTime? fromSatrtDate;
+
+        public DateTime? FromSatrtDate
+        {
+            get { return fromSatrtDate; }
+            set { fromSatrtDate = value; }
+        }
+
+        DateTime? toSatrtDate;
+
+        public DateTime? ToSatrtDate
+        {
+            get { return toSatrtDate; }
+            set { toSatrtDate = value; }
+        }
+
+
+
         public bool IsStartDateChanged
         {
             get { return isStartDateChanged; }
@@ -27,6 +61,22 @@ namespace PMTool.Models
         {
             get { return isSatausChanged; }
             set { isSatausChanged = value; }
+        }
+
+        string fromSattus;
+
+        public string FromSataus
+        {
+            get { return fromSattus; }
+            set { fromSattus = value; }
+        }
+
+        string toStatus;
+
+        public string ToStatus
+        {
+            get { return toStatus; }
+            set { toStatus = value; }
         }
     }
 }
