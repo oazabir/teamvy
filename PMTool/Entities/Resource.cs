898b8efd1185864da4015d9d39d77c1a2eb11b7c
najibcse <najib.cse@gmail.com>
2014-03-28 15:29:31 +0600

    dsfsdf

---
+++ PMTool/Entities/Resource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PMTool.Entities
+{
+    public class Resource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Operations Operations { get; set; }
+    }
+}
---
+++ PMTool/Entities/UserProfile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using PMTool.Models;
+
+
+    [Table("UserProfile")]
+    public class UserProfile
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+        public string NewProp { get; set; }
+
+        public virtual List<Project> Projects { get; set; }
+
+        public virtual List<Task> Tasks { get; set; }
+
+        public virtual List<Task> FollowerTasks { get; set; }
+
+        //New added for Project Owner
+        public virtual List<Project> OwnerProjects { get; set; }
+    }
+
+
