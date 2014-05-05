89b1066a2ada2aa1fb50a4abd1288de6b993c794
mahedee <mahedee.hasan@gmail.com>
2014-05-05 15:30:37 +0600

    Commit

---
+++ PMTool/Migrations/201405050929357_initalmigration.cs
@@ -0,0 +1,16 @@
+namespace PMTool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initalmigration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
