25d35518350a39e5322d04e41014477665301638
najibcse <najibcse@gmail.com>
2014-05-06 17:08:04 +0600

    CommitedByBadhon

---
+++ PMTool/Models/SearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PMTool.Models
+{
+    public class SearchViewModel
+    {
+        public long? SelectedProjectID { get; set; }
+        public long? SelectedStatusID { get; set; }
+        
+    }
+}
