f557fa485653d74cbd1245313900ff5182d5b971
najibcse <najibcse@gmail.com>
2014-05-23 18:03:16 +0600

    Commit By Badhon

---
+++ PMTool/Models/BurndownChartViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PMTool.Models
+{
+    public class BurndownChartViewModel
+    {
+        public long ProjectID { get; set; }
+    }
+}
